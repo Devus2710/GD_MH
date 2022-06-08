e266607c7c8ce36f9528892bec3016b3a03743ed
Life is squid <vetrov.arts1704@gmail.com>
2022-06-08 12:24:50 +0300

    create and update function 3

--- GD_MathHelp_lab_code/MathHelper.cs
+++ GD_MathHelp_lab_code/MathHelper.cs
@@ -105,5 +105,54 @@ namespace GD_MathHelp_lab_code
 			setText(solutionHistory.ToString());
 			return result;
 		}
+
+		//      public double Func3(double n, SetTextDelegate setText)// рекурсия, долго работает
+		//{
+		//	if (n == 1)
+		//	{
+		//		setText("1/1");
+		//		return 1;
+		//	}
+		//	setText($"{1}/{n} + ");
+
+		//	return Math.Round(1 / n + Func3(n - 1, setText), 5);
+		//}
+
+		public double Func3(double end, SetTextDelegate setText) // без рекурсии
+		{
+			solutionHistory = new StringBuilder();
+
+			if (end == 1)
+			{
+				solutionHistory.Append("1/1");
+				setText(solutionHistory.ToString());
+				return 1;
+			}
+
+			double n = 1;
+			double result = 0;
+
+			while (n <= end)
+			{
+				result += 1 / n;
+
+				if (n != 1)
+				{
+					solutionHistory.Append($" + ");
+				}
+				solutionHistory.Append($"{1}/{n}");
+
+				if (n > format) // форматирование для сохранения в текстовый файл
+				{
+					solutionHistory.AppendLine();
+					format += format;
+				}
+
+				n++;
+			}
+
+			setText(solutionHistory.ToString());
+			return Math.Round(result, 5);
+		}
 	}
 }
