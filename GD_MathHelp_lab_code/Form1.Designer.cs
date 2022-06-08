1bd3abfaedd3b34c72ef816bf8d7d00dc408fac2
Дима Ветров <56885717+Devus2710@users.noreply.github.com>
2022-06-08 12:22:02 +0300

    Form1_update

--- GD_MathHelp_lab_code/Form1.Designer.cs
+++ GD_MathHelp_lab_code/Form1.Designer.cs
@@ -56,9 +56,10 @@ namespace GD_MathHelp_lab_code
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton1.Location = new System.Drawing.Point(27, 107);
+            this.radioButton1.Location = new System.Drawing.Point(40, 165);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(221, 20);
+            this.radioButton1.Size = new System.Drawing.Size(344, 29);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "res = 1-sqrt(2)+3-sqrt(4)+...+sqrt(n)";
@@ -68,9 +69,10 @@ namespace GD_MathHelp_lab_code
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton2.Location = new System.Drawing.Point(27, 130);
+            this.radioButton2.Location = new System.Drawing.Point(40, 200);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(176, 20);
+            this.radioButton2.Size = new System.Drawing.Size(260, 29);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "res = 1^1+2^2+3^3+..+n^n";
@@ -80,9 +82,10 @@ namespace GD_MathHelp_lab_code
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton3.Location = new System.Drawing.Point(27, 153);
+            this.radioButton3.Location = new System.Drawing.Point(40, 235);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(192, 20);
+            this.radioButton3.Size = new System.Drawing.Size(301, 29);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "res = 1/1+1/2+1/3+1/4+...+1/n\r\n";
@@ -90,40 +93,45 @@ namespace GD_MathHelp_lab_code
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(27, 344);
+            this.richTextBox1.Location = new System.Drawing.Point(40, 529);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(729, 99);
+            this.richTextBox1.Size = new System.Drawing.Size(1092, 150);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(535, 107);
+            this.textBox1.Location = new System.Drawing.Point(802, 165);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.Size = new System.Drawing.Size(220, 26);
             this.textBox1.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(27, 190);
+            this.button1.Location = new System.Drawing.Point(40, 292);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 40);
+            this.button1.Size = new System.Drawing.Size(156, 62);
             this.button1.TabIndex = 5;
             this.button1.Text = "Посчитать";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // maskedTextBox1
             // 
             this.maskedTextBox1.HidePromptOnLeave = true;
-            this.maskedTextBox1.Location = new System.Drawing.Point(337, 108);
+            this.maskedTextBox1.Location = new System.Drawing.Point(506, 166);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.maskedTextBox1.Mask = "00000";
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(115, 20);
+            this.maskedTextBox1.Size = new System.Drawing.Size(170, 26);
             this.maskedTextBox1.TabIndex = 6;
             this.maskedTextBox1.ValidatingType = typeof(int);
             // 
@@ -131,9 +139,10 @@ namespace GD_MathHelp_lab_code
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(24, 58);
+            this.label1.Location = new System.Drawing.Point(36, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 16);
+            this.label1.Size = new System.Drawing.Size(244, 25);
             this.label1.TabIndex = 7;
             this.label1.Text = "Формулы для расчета";
             // 
@@ -141,9 +150,10 @@ namespace GD_MathHelp_lab_code
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(334, 58);
+            this.label2.Location = new System.Drawing.Point(501, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 16);
+            this.label2.Size = new System.Drawing.Size(163, 25);
             this.label2.TabIndex = 8;
             this.label2.Text = "Входное число";
             // 
@@ -151,9 +161,10 @@ namespace GD_MathHelp_lab_code
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(24, 310);
+            this.label3.Location = new System.Drawing.Point(36, 477);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(138, 16);
+            this.label3.Size = new System.Drawing.Size(194, 25);
             this.label3.TabIndex = 9;
             this.label3.Text = "История решений";
             // 
@@ -161,21 +172,24 @@ namespace GD_MathHelp_lab_code
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(532, 58);
+            this.label4.Location = new System.Drawing.Point(798, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 16);
+            this.label4.Size = new System.Drawing.Size(120, 25);
             this.label4.TabIndex = 10;
             this.label4.Text = "Результат";
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(628, 298);
+            this.button2.Location = new System.Drawing.Point(942, 458);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 40);
+            this.button2.Size = new System.Drawing.Size(192, 62);
             this.button2.TabIndex = 11;
             this.button2.Text = "Сохранить историю в текстовый файл";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // saveFileDialog1
             // 
@@ -184,15 +198,18 @@ namespace GD_MathHelp_lab_code
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(491, 298);
+            this.button3.Location = new System.Drawing.Point(736, 458);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(128, 40);
+            this.button3.Size = new System.Drawing.Size(192, 62);
             this.button3.TabIndex = 12;
             this.button3.Text = "Очистить историю";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
@@ -202,44 +219,48 @@ namespace GD_MathHelp_lab_code
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.LightGray;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(97, 9);
+            this.label5.Location = new System.Drawing.Point(146, 14);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(570, 24);
+            this.label5.Size = new System.Drawing.Size(865, 33);
             this.label5.TabIndex = 15;
             this.label5.Text = "ПОВЫШЕНИЕ ЭФФЕКТИВНОСТИ ТРУДА МАТЕМАТИКОВ";
             // 
             // panel1
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.LightGray;
-            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(768, 45);
+            this.panel1.Size = new System.Drawing.Size(1152, 69);
             this.panel1.TabIndex = 16;
             // 
             // statusStrip1
             // 
             this.statusStrip1.BackColor = System.Drawing.Color.LightGray;
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 459);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 708);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(768, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1152, 32);
             this.statusStrip1.TabIndex = 17;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(338, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(495, 25);
             this.toolStripStatusLabel1.Text = "Выполнили: Врубливский Артем, Палагута Михаил. ПИ-20а";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(768, 481);
+            this.ClientSize = new System.Drawing.Size(1152, 740);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.panel1);
@@ -256,6 +277,7 @@ namespace GD_MathHelp_lab_code
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Повышаем эффективность труда математиков";
             this.statusStrip1.ResumeLayout(false);
--- GD_MathHelp_lab_code/Form1.cs
+++ GD_MathHelp_lab_code/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,80 @@ namespace GD_MathHelp_lab_code
 {
     public partial class Form1 : Form
     {
+        //private Class1 Solver = new Class1();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (!Int32.TryParse(maskedTextBox1.Text, out int value) || value < 1)
+            {
+                MessageBox.Show($"Входные данные не являются корректными: {maskedTextBox1.Text}", "ERROR!");
+                return;
+            }
+
+            textBox1.Text = await StartWork(value);
+            SetTextToBox($" = {textBox1.Text}\n\n");
+            MessageBox.Show($"Подсчет окончен\nРезультат: {textBox1.Text}");
+        }
+
+        private void SetTextToBox(string text) => richTextBox1.Invoke(new Action(() => richTextBox1.Text += text));
+
+        private async Task<string> StartWork(int value)
+        {
+            //Solver = new Class1();
+            return await Task.Run(() =>
+            {
+                string resultString = "";
+                double result = 0.0;
+
+                if (radioButton1.Checked)
+                {
+                    //result = Solver.Func1(value, new SetTextDelegate(SetTextToBox));
+                    resultString = result.ToString();
+                }
+                else if (radioButton2.Checked)
+                {
+                    //resultString = Solver.Func2(value, new SetTextDelegate(SetTextToBox)).ToString();
+                }
+                else if (radioButton3.Checked)
+                {
+                    //resultString = Solver.Func3(value, new SetTextDelegate(SetTextToBox)).ToString();
+                }
+
+                return resultString;
+            });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(richTextBox1.Text))
+            {
+                MessageBox.Show("Недостаточно данных для сохранения!");
+                return;
+            }
+
+            DialogResult dialogResult = saveFileDialog1.ShowDialog();
+            if (dialogResult != DialogResult.OK && dialogResult != DialogResult.Yes) return;
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    writer.Write(richTextBox1.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Невозможно записать данные в файл. {ex.Message}");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
         }
     }
 }
