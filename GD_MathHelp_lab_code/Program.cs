96c4b2f14e0d1acfdc38530222045a91af421545
Дима Ветров <56885717+Devus2710@users.noreply.github.com>
2022-06-08 12:07:40 +0300

    Create_project

---
+++ GD_MathHelp_lab_code/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GD_MathHelp_lab_code
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
